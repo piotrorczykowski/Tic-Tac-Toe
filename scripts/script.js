26a34b814cbe2419559551f5d70139bfb0a450bf
Piotr Orczykowski <orczykowski.peter@gmail.com>
2021-08-12 11:22:27 +0200

    Add function to check who won

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 var turn = 0;
+var xCellCliked = '';
+var oCellCliked = '';
 
 function clicked(id)
 {
@@ -7,9 +9,25 @@ function clicked(id)
     if(cell.value !== '') return;
 
     if(turn % 2 === 0)
+    {
         cell.value = 'x';
+        xCellCliked += id
+    }
     else
+    {
         cell.value = 'o';
+        oCellCliked += id;
+    }
     
     turn++;
+    check();
+}
+
+function check()
+{
+    var winningCombinations = ['123','456','789','147','258','369','159','357']
+
+    winningCombinations.forEach(element => {
+        alert(xCellCliked.includes(element));
+    });
 }
